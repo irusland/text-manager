807a479bb596ecded13a5cbe51155f5244da3554
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2020-03-07 01:03:39 +0500

    map<string, int> added, comparison done

--- smap.cpp
+++ smap.cpp
@@ -1,41 +1,26 @@
+//
+// Created by Ruslan Sirazhetdinov on 07.03.2020.
+//
+
 #include <iostream>
 #include <fstream>
 #include <set>
+#include <map>
 #include <chrono>
+#include <string>
 
 using namespace std;
 
-
-class Pair {
-public:
-    Pair(const char *key, int count) : key(key), count(count) {}
-    const char *key;
-    mutable int count;
-};
-
-struct LexicalComparator {
-    typedef std::true_type is_transparent;
-    bool operator()(const Pair& lhs, const char *rhs) const {
-        return strcmp(lhs.key, rhs) < 0;
-    }
-    bool operator()(const char *lhs, const Pair& rhs) const {
-        return strcmp(lhs, rhs.key) < 0;
-    }
-    bool operator()(const Pair& lhs, const Pair& rhs) const {
-        return strcmp(lhs.key, rhs.key) < 0;
-    }
-};
-
 struct CountComparator {
     typedef std::true_type is_transparent;
-    bool operator()(const Pair& lhs, const int rhs) const {
-        return lhs.count < rhs;
+    bool operator()(const pair<string, int>& lhs, const int rhs) const {
+        return lhs.second < rhs;
     }
-    bool operator()(const int lhs, const Pair& rhs) const {
-        return lhs < rhs.count;
+    bool operator()(const int lhs, const pair<string, int>& rhs) const {
+        return lhs < rhs.second;
     }
-    bool operator()(const Pair& lhs, const Pair& rhs) const {
-        return lhs.count < rhs.count;
+    bool operator()(const pair<string, int>& lhs, const pair<string, int>& rhs) const {
+        return lhs.second < rhs.second;
     }
 };
 
@@ -62,13 +47,13 @@ int main(int argc, char **argv) {
 
     Text[size] = '\0';
 
-    set<Pair, LexicalComparator> word_counter; // word sorting
-    multiset<Pair, CountComparator> count_sorted; // sorting by count
+    map<string, int> word_counter;
+    multiset<pair<string, int>, CountComparator> count_sorted; // sorting by count
 
     auto start = chrono::steady_clock::now();
 
     size_t wordlen = 0;
-    char *word = (char*)malloc(wordlen);
+    string word;
     for (int i = 0; i <= size; ++i) {
         if (!isalpha(Text[i])) {
             if (wordlen == 0) {
@@ -76,16 +61,15 @@ int main(int argc, char **argv) {
             }
             auto it = word_counter.find(word);
             if (it == word_counter.end()) {
-                word_counter.insert(Pair(word, 1));
+                word_counter[word] = 1;
             } else {
-                it->count++;
+                it->second++;
             }
             wordlen = 0;
-            word = (char*)malloc(wordlen);
+            word = "";
         } else {
             wordlen++;
-            word = (char*)realloc(word, wordlen);
-            word[wordlen - 1] = (char)tolower((int)Text[i]);
+            word += (char)tolower((int)Text[i]);
         }
     }
 
@@ -96,7 +80,7 @@ int main(int argc, char **argv) {
     auto end = chrono::steady_clock::now();
 
     for (auto element : count_sorted)
-        std::cout << element.key << " - " << element.count << std::endl;
+        std::cout << element.first << " - " << element.second << std::endl;
     cout << "File size " << size << " B" << endl;
     cout << "Elapsed " << chrono::duration_cast<chrono::milliseconds>(end - start).count()
          << " ms" << endl;
